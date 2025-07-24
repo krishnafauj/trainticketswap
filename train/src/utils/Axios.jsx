de1fada5a7ec0a20edf90f18e3a9e0a15b4bcafe
krishnafauj <krishnafauj02@gmail.com>
2025-07-24 21:42:23 +0530

    error

--- train/src/utils/Axios.jsx
+++ train/src/utils/Axios.jsx
@@ -2,17 +2,22 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api', 
+  baseURL: '/api',
   timeout: 10000, // optional: 10 seconds
   headers: {
     'Content-Type': 'application/json',
   },
 });
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); 
-  if (token) {
+  const token = localStorage.getItem('token');
+
+  if (token && token !== 'null' && token !== 'undefined') {
     config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    // Remove Authorization header completely if no valid token
+    delete config.headers.Authorization;
   }
+
   return config;
 });
 
